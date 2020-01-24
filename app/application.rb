d6b100fed204a3f6663860e1a4a07dcbed243806
Zach Weber <weberzt@gmail.com>
2020-01-24 17:15:17 -0600

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+    def call(env)
+        resp = Rack::Response.new
+
+        if Time.now.hour.between?(0, 11)
+            resp.write "Good morning!"
+        elsif Time.now.hour.between?(12, 23)
+            resp.write "Good Afternoon!"
+        end
+        resp.finish
+    end
+end
